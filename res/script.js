5b838c70b09d3be7914e1d1e78a507de567e9837
matildawe <63040887+matildawe@users.noreply.github.com>
2020-12-13 16:58:27 +0100

    visitor counter test

--- res/script.js
+++ res/script.js
@@ -1,7 +1,14 @@
 /* This JavaScript need jQuery to run */
 $(document).ready(function() { //JavaScriptet nedan körs när HTML-sidan har laddat klart. 
 
-    
+        
+      function counter() { 
+        var counterElement = document.getElementById("cntr")
+    var counterNumber = parseInt(counterElement.innerHTML)
+    counterNumber = counterNumber + 1
+    counterElement.innerHTML = counterNumber
+    }
+    counter();
 
     //Döljer alla sidor när sidan laddas
     function hidePageSections() { 
